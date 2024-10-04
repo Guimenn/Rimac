6cff4bc2ca548635c24d3ff578f5d4d3bbd8dc97
palmitex <gabriel1208.palmieri@gmail.com>
2024-10-04 14:12:42 -0300

    teste

--- ativ/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // Verifica se o botão foi clicado
-    if (isset($_POST['Continuar'])) {
-        // Redireciona para outro arquivo HTML ou site
-        header("Location: index.html");
-        exit(); // Sempre use exit após o redirecionamento.
-    }
-?>
