1de795ad579ffd7e8f28009b10fab4d44de072d2
Guilherme Vidichosqui Men <166948520+Guimenn@users.noreply.github.com>
2024-10-03 22:53:43 -0300

    Sobrenos att

--- ativ/login.php
+++ ativ/login.php
@@ -2,7 +2,7 @@
     // Verifica se o botão foi clicado
     if (isset($_POST['Continuar'])) {
         // Redireciona para outro arquivo HTML ou site
-        header("Location: ../index.html");
+        header("Location: index.html");
         exit(); // Sempre use exit após o redirecionamento.
     }
 ?>
